da6365de36cdaea783e6aba1fa537ec2b4788b9c
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2020-07-13 21:49:38 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -22,9 +22,9 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:25210" >>${CONF}
+  echo "listen = 0.0.0.0:8080" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
-  echo "obfs = tls" >>${CONF}
+  echo "obfs = http" >>${CONF}
 fi
 if [ -f ${SYSTEMD} ]; then
   echo "Found existing service..."
